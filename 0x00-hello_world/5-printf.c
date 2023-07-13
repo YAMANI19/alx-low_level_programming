1c2c6e33898a81b876931e7f56d3123e337ed817
Mohamed Yamani <ymnsimo19@gmail.com>
2023-07-13 14:38:26 -0700

    s

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
  * main - Entry point
- * Description: C program that print whit puts fonction
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-        puts("with proper grammar, but the outcome is a piece of art,/n");
+        printf("with proper grammar, but the outcome is a piece of art,/n");
         return (0);
 }
