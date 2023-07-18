b59ba1c65ee8d2925e85b8d812cd0c9ae0fa0f0a
Mohamed Yamani <ymnsimo19@gmail.com>
2023-07-18 12:20:28 -0700

    nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,11 @@
   */
 void print_alphabet_x10(void)
 {
-	int i;
 	int j;
 
-	for (j = 1; i <= 10; i++)
-	{
-		for (j = 97; j <= 122; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
 		_putchar('\n');
-	}
 }
