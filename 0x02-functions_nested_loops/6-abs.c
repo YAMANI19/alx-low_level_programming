3043ff5ae2daf21114b517e35eb7ffafb3fe087f
Mohamed Yamani <ymnsimo19@gmail.com>
2023-07-18 12:59:06 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * _abs - Computes the absolute value of an integer.
+  * @c: The number to be computed.
+  *
+  * Return: Absolute value of number or zero
+  */
+int _abc(int c)
+{
+	if (c < 0)
+	{
+		int abc_val;
+
+		abc_val = c * -1;
+
+		return (abc_val);
+	}
+
+	return (c);
+}
