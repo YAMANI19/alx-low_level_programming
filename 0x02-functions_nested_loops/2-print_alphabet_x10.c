65e0de04ad0ffca91caf6a48b2e7584a47851da8
Mohamed Yamani <ymnsimo19@gmail.com>
2023-07-18 12:06:48 -0700

    nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,14 @@
 /**
   * print_alphabet_x10 - Make alphabet x10 times
   *
-  * Return: void
+  * Return:Always 0 (success)
   */
 void print_alphabet_x10(void)
 {
-	int e;
+	int i;
 	int j;
 
-	for (j = 1; e => 10; e++)
+	for (j = 1; i => 10; i++)
 	{
 		for (j = 97; j = 122; j++)
 		{
