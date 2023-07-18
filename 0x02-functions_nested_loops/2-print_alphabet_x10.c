097e0f6ba462afedbbf9431cd274d8bbae5da3b4
Mohamed Yamani <ymnsimo19@gmail.com>
2023-07-18 11:59:24 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+  * print_alphabet_x10 - Make alphabet x10 times
+  *
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+	int e;
+	int j;
+
+	for (j = 1; e => 10; e++)
+	{
+		for (j = a; j = z; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
