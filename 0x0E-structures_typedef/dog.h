1978e54fa54614cbb996a556105aba252f24965f
Mohamed Yamani <ymnsimo19@gmail.com>
2023-08-15 17:13:29 -0700

    macro

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for dog info
+ * @name: type char dog's name
+ * @age: float type dog's age
+ * @owner: char type owner's name
+ *
+ *Description: structure to store dog info
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
