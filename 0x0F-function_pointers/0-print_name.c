ed40cb4698127966ab57d6040b15cdf138c028b6
Mohamed Yamani <ymnsimo19@gmail.com>
2023-08-16 16:35:51 -0700

    fun pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,11 @@
  * print_name - Print a name from a function pointer
  * @name: char string
  * @f: function pointer that takes a string argument
+ *
  * return: void
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if(name && f)
+	if (name && f)
 		f(name);
 }
